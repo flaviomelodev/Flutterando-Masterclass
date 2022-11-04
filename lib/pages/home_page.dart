ff788461518f19bdece8c45201f67dc9b4f1b4f9
Flávio Melo <hastnumber1@gmail.com>
2022-11-04 10:09:21 -0300

    home_page
    Trocada a forma de apresentação dos desafios

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,26 @@
+import 'package:desafio1_masterclass/pages/challenge_1.dart';
+import 'package:desafio1_masterclass/pages/challenge_2.dart';
+import 'package:desafio1_masterclass/pages/challenge_3.dart';
+import 'package:desafio1_masterclass/widget/logo_challenge_1.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final PageController controller = PageController(
+      initialPage: 0,
+    );
+    return PageView(
+      scrollDirection: Axis.horizontal,
+      controller: controller,
+      children: const [
+        Challenge1(),
+        Challenge2(),
+        Challenge3(),
+      ],
+    );
+  }
+}
